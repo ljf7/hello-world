1e862160987d149ba7ebc15fd67190ef77692f6d
ljf7 <ljf7417@qq.com>
2019-09-27 17:50:30 +0800

    test

---
+++ test.php
@@ -0,0 +1,21 @@
+<?
+require 'vendor/autoload.php'; //自动加载类库
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// create a log channel
+$log = new Logger('SQL');
+$date = date('Y-m-d');
+
+//'app/logs/sql_'.$date.'.log'  路径以及日志文件名
+
+//Logger::WARNING 日志级别
+$log->pushHandler(new StreamHandler('app/logs/sql_'.$date.'.log', Logger::WARNING));
+
+$error_str = '['.date('Y-m-d H:i:s').']';
+$error_str .= 'this is error';
+
+// add records to the log
+$log->addWarning('Foo');
+$log->addError($error_str);
